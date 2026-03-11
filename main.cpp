18fcd5dfbbc156c91e83c7aeadd01989e7795c6e
Sam <scorpionsam99@gmail.com>
2026-03-10 21:34:43 -0700

    a35 done

--- main.cpp
+++ main.cpp
@@ -9,15 +9,13 @@ int main()
     int rdnum1, rdnum2, rdnum3;
     int total = 0;
 
-    // TODO: seed the random number generator with time(0)
-
-    // TODO: generate 3 random numbers between 0 and 99
-
-    // TODO: compute the total summation
-
-    // TODO: print the 3 random numbers on the first line
-
-    // TODO: print the total and average (integer division) on the second line
-
+    srand(time(0));
+    rdnum1 = rand() % 100;
+    rdnum2 = rand() % 100;
+    rdnum3 = rand() % 100;
+    total = rdnum1 + rdnum2 + rdnum3;
+    
+    cout << rdnum1 << " " << rdnum2 << " " << rdnum3 << endl;
+    cout << total << " " << total / 3 << endl;
     return 0;
 }
